4037b0222812e7ff2db49297978904893c07730d
Ben Day <bjd39@cam.ac.uk>
2018-07-24 13:39:42 +0200

    Added activation option and simplified GAT layer

--- layers.py
+++ layers.py
@@ -1,10 +1,10 @@
 import torch.nn as nn
 import torch
 import torch.nn.functional as F
-from mechanism import ConditionalAttentionMech
+from mechanisms import ConditionalAttentionMech, SimplifiedGAT
 
 class ConditionalAttentionLayer(nn.Module):
-    def __init__(self, ins, outs, dropout, leak, N_mechs, concat=True):
+    def __init__(self, ins, outs, dropout, leak, N_mechs, concat=True, activation=F.elu):
         super(ConditionalAttentionLayer, self).__init__()
         
         self.dropout = dropout
@@ -15,6 +15,8 @@ class ConditionalAttentionLayer(nn.Module):
         for i, mechanism in enumerate(self.mechanisms):
             self.add_module('mechanism_{}'.format(i), mechanism)
         
+        self.activation = activation
+        
     def forward(self, x, adj):
         x = F.dropout(x, self.dropout, training=self.training)
         # either concatenate (intermediate layer)
@@ -24,5 +26,31 @@ class ConditionalAttentionLayer(nn.Module):
         else:
             x = torch.sum([mech(x, adj) for mech in self.mechanisms], dim=1)
         x = F.dropout(x, self.dropout, training=self.training)
-        x = F.elu(self.out_att(x, adj))
+        x = activation(self.out_att(x, adj))
+        return x
+
+class SimplifiedGATLayer(nn.Module):
+    def __init__(self, ins, outs, dropout, leak, N_mechs, concat=True, activation=F.elu):
+        super(SimplifiedGATLayer, self).__init__()
+        
+        self.dropout = dropout
+        
+        # generate the N mechanisms
+        self.mechanisms = [SimplifiedGAT(ins,outs,dropout,leak) for _ in range(N_mechs)]
+        # add to module
+        for i, mechanism in enumerate(self.mechanisms):
+            self.add_module('mechanism_{}'.format(i), mechanism)
+        
+        self.activation = activation
+        
+    def forward(self, x, adj):
+        x = F.dropout(x, self.dropout, training=self.training)
+        # either concatenate (intermediate layer)
+        if concat:
+            x = torch.cat([mech(x, adj) for mech in self.mechanisms], dim=1)
+        # or sum (final layer)
+        else:
+            x = torch.sum([mech(x, adj) for mech in self.mechanisms], dim=1)
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = activation(self.out_att(x, adj))
         return x
