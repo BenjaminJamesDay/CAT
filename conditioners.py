d2f1dc09fd91b95de522ea63453a613f4d0dd769
Ben Day <bjd39@cam.ac.uk>
2018-09-11 17:21:04 +0100

    missing self.

--- conditioners.py
+++ conditioners.py
@@ -16,7 +16,7 @@ class coraConditionerModel(nn.Module):
         self.hidden2 = nn.Linear(64,32)
         self.hidden3 = nn.Linear(32,16)
         
-        nn.init.xavier_uniform_(hidden3.weight, 0.05)
+        nn.init.xavier_uniform_(self.hidden3.weight, 0.05)
 
     def forward(self, x):
         x = F.relu(self.hidden1(x))
