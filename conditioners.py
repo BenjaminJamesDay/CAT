604bb0aa08fd04a3875f685d18249fa1ccfc1bf5
Ben Day <bjd39@cam.ac.uk>
2018-09-11 17:26:36 +0100

    network change

--- conditioners.py
+++ conditioners.py
@@ -12,14 +12,16 @@ Included conditioners:
 class coraConditionerModel(nn.Module):
     def __init__(self):
         super(coraConditionerModel, self).__init__()
-        self.hidden1 = nn.Linear(1433,32)
-        self.hidden2 = nn.Linear(32,16)
+        self.hidden1 = nn.Linear(1433,64)
+        self.hidden2 = nn.Linear(64,32)
+        self.hidden3 = nn.Linear(32,16)
         
-        nn.init.xavier_uniform_(self.hidden2.weight, 0.05)
+        nn.init.xavier_uniform_(self.hidden3.weight, 0.05)
 
     def forward(self, x):
         x = F.relu(self.hidden1(x))
-        return (self.hidden2(x))
+        x = F.relu(self.hidden2(x))
+        return (self.hidden3(x))
 
 def coraConditioner(x):
     """
