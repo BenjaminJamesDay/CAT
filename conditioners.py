73ab07db37a78e7373ef349a1c4420f7c53740b9
Ben Day <bjd39@cam.ac.uk>
2018-09-12 09:58:51 +0100

    Adding an ELU version and changed to reflect the numebr of mechs in the new model - really need to make these talk to each other later

--- conditioners.py
+++ conditioners.py
@@ -14,7 +14,7 @@ class coraConditionerModel(nn.Module):
         super(coraConditionerModel, self).__init__()
         self.hidden1 = nn.Linear(1433,64)
         self.hidden2 = nn.Linear(64,32)
-        self.hidden3 = nn.Linear(32,32)
+        self.hidden3 = nn.Linear(32,24)
         
         nn.init.xavier_uniform_(self.hidden3.weight, 0.05)
 
@@ -36,4 +36,33 @@ def coraConditioner(x):
     model.cuda()
     cond = torch.cat([model(example) for example in x])
     #reshape the concatenated set (2N.N_mechs) to (N*2*N_mechs) then permute to (2*N_mechs*N)
-    return cond.view(2708,2,16).permute(1,2,0)
+    return cond.view(2708,2,12).permute(1,2,0)
+
+class coraConditionerModel2(nn.Module):
+    def __init__(self):
+        super(coraConditionerModel, self).__init__()
+        self.hidden1 = nn.Linear(1433,64)
+        self.hidden2 = nn.Linear(64,32)
+        self.hidden3 = nn.Linear(32,24)
+        
+        nn.init.xavier_uniform_(self.hidden3.weight, 0.05)
+
+    def forward(self, x):
+        x = F.elu(self.hidden1(x))
+        x = F.elu(self.hidden2(x))
+        return (self.hidden3(x))
+
+def coraConditioner2(x):
+    """
+    Input is the Cora dataset 2708*1433
+    For each node (2708) the features (1433) are used to generate conditioning
+    parameters (16 = 8*2) in parallel. That is, the same network is used to
+    transform the features to the conditioning parameters for every node. Thus
+    the network itself is 1433->..->16
+    """
+    
+    model = coraConditionerModel2()
+    model.cuda()
+    cond = torch.cat([model(example) for example in x])
+    #reshape the concatenated set (2N.N_mechs) to (N*2*N_mechs) then permute to (2*N_mechs*N)
+    return cond.view(2708,2,12).permute(1,2,0)
