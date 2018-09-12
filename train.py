cf1b4583475729d9af0bd71136e8b91a52d39168
Ben Day <bjd39@cam.ac.uk>
2018-09-12 10:00:42 +0100

    Switched to new (2nd) model in training setup

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ from torch.autograd import Variable
 
 from utils import load_data, accuracy
 from models import CCModel
-from conditioners import coraConditioner
+from conditioners import coraConditioner, coraConditioner2
 
 # Training settings
 parser = argparse.ArgumentParser()
@@ -49,7 +49,7 @@ if args.cuda:
 adj, features, labels, idx_train, idx_val, idx_test = load_data()
 
 # Model and optimizer
-model = CCModel(classes=int(labels.max()) + 1, ins=features.shape[1], conditioner=coraConditioner)
+model = CCModel2(classes=int(labels.max()) + 1, ins=features.shape[1], conditioner=coraConditioner2)
 optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
 if args.cuda:
