127aada2b354c1f4aab69d8afed8fe1ab1c84992
Ben Day <bjd39@cam.ac.uk>
2018-09-13 11:34:21 +0100

    Only saving best epoch

--- train.py
+++ train.py
@@ -113,7 +113,7 @@ for epoch in range(args.epochs):
     if acc_values[-1] >= best:
         best = acc_values[-1]
         best_epoch = epoch
-        torch.save(model.state_dict(), '{}.pkl'.format(epoch))
+        torch.save(model.state_dict(), 'best_model.pkl')
         bad_counter = 0
     else:
         bad_counter += 1
@@ -122,24 +122,12 @@ for epoch in range(args.epochs):
         print(best)
         break
 
-    files = glob.glob('*.pkl')
-    for file in files:
-        epoch_nb = int(file.split('.')[0])
-        if epoch_nb < best_epoch:
-            os.remove(file)
-
-files = glob.glob('*.pkl')
-for file in files:
-    epoch_nb = int(file.split('.')[0])
-    if epoch_nb > best_epoch:
-        os.remove(file)
-
 print("Optimization Finished!")
 print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
 
 # Restore best model
 print('Loading {}th epoch'.format(best_epoch))
-model.load_state_dict(torch.load('{}.pkl'.format(best_epoch)))
+model.load_state_dict(torch.load('best_model.pkl'))
 
 # Testing
 compute_test()
