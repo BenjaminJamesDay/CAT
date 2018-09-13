2c36d85630f054c4da6e13e9c8d8fc51d6b12d22
Ben Day <bjd39@cam.ac.uk>
2018-09-13 11:06:24 +0100

    Switched to early stopping on val_acc (as in GAT paper) with fixed conditioning

--- train.py
+++ train.py
@@ -88,7 +88,7 @@ def train(epoch):
           'acc_val: {:.4f}'.format(acc_val.data[0]),
           'time: {:.4f}s'.format(time.time() - t))
 
-    return loss_val.data[0]
+    return acc_val.data[0]
 
 
 def compute_test():
@@ -103,16 +103,16 @@ def compute_test():
 
 # Train model
 t_total = time.time()
-loss_values = []
+acc_values = []
 bad_counter = 0
-best = args.epochs + 1
+best = 0
 best_epoch = 0
 for epoch in range(args.epochs):
-    loss_values.append(train(epoch))
+    acc_values.append(train(epoch))
 
     torch.save(model.state_dict(), '{}.pkl'.format(epoch))
-    if loss_values[-1] < best:
-        best = loss_values[-1]
+    if acc_values[-1] > best:
+        best = acc_values[-1]
         best_epoch = epoch
         bad_counter = 0
     else:
