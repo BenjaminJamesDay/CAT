0ff55189e754e844412a76cc81b708bde2caf96d
BenjaminJamesDay <bjd39@cam.ac.uk>
2018-09-14 08:20:33 +0100

    Changed random seed & dropout

--- models.py
+++ models.py
@@ -26,7 +26,7 @@ class CCModel(nn.Module):
         self.CAT1 = ConditionalAttentionLayer(ins=ins, outs=8, dropout=0.3, leak=0.2, N_mechs=8,
                                               conditioner=coraConditioner2, activate=True)
         # do not activate the output
-        self.CAT2 = UnconditionalAttentionLayer(N_mechs=1, dropout=0.3, ins=64, leak=0.2, outs=classes)
+        self.CAT2 = UnconditionalAttentionLayer(N_mechs=1, dropout=0.4, ins=64, leak=0.2, outs=classes)
         
     def forward(self, x, adj):
         # pass is v tidy, just first layer then second
